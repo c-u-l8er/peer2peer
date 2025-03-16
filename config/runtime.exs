50ce48b51d9bef95ed3a454b84b129a4cb79ba43
Travis Burandt <travis.burandt@gmail.com>
2025-03-16 15:38:11 -0500

    server true

--- config/runtime.exs
+++ config/runtime.exs
@@ -61,15 +61,14 @@ if config_env() == :prod do
   config :peer2peer, :dns_cluster_query, System.get_env("DNS_CLUSTER_QUERY")
 
   config :peer2peer, Peer2peerWeb.Endpoint,
-    url: [host: host, port: 443, scheme: "https"],
+    url: [host: System.get_env("PHX_HOST") || "peer2peer.fly.dev", port: 443, scheme: "https"],
     http: [
-      # Enable IPv6 and bind on all interfaces.
-      # Set it to  {0, 0, 0, 0, 0, 0, 0, 1} for local network only access.
-      # See the documentation on https://hexdocs.pm/bandit/Bandit.html#t:options/0
-      # for details about using IPv6 vs IPv4 and loopback vs public addresses.
-      ip: {0, 0, 0, 0, 0, 0, 0, 0},
-      port: port
+      # Listen on all available network interfaces
+      ip: {0, 0, 0, 0},
+      port: String.to_integer(System.get_env("PORT") || "8080")
     ],
+    # <- This is crucial - it tells Phoenix to start the HTTP server
+    server: true,
     secret_key_base: secret_key_base
 
   # ## SSL Support
